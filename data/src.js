89ece8f52d03cf7631f0af65ef3ad43dba5a15db
FKee <83258863+f1041527230@users.noreply.github.com>
2025-03-11 15:55:28 +0800

    init

---
+++ data/src.js
@@ -0,0 +1,131 @@
+let songs = [
+    {
+        "Id": 543,
+        "Name": "○+●",
+        "Singer": "kous,ef",
+        "Lang": "日语",
+        "MasterPoints": 4,
+        "Notes": "",
+        "DelStatus": 0,
+        "Categories": [
+            "日系流行（J-pop）",
+            "术曲(Vocaloid)"
+        ]
+    },
+    {
+        "Id": 972,
+        "Name": "01",
+        "Singer": "女王蜂",
+        "Lang": "日语",
+        "MasterPoints": 3,
+        "Notes": "",
+        "DelStatus": 0,
+        "Categories": [
+            "日系摇滚（J-rock）",
+            "动漫（Anime）"
+        ]
+    },
+    {
+        "Id": 1076,
+        "Name": "10:30 的飞机场",
+        "Singer": "陶喆",
+        "Lang": "华语",
+        "MasterPoints": 4,
+        "Notes": "",
+        "DelStatus": 0,
+        "Categories": [
+            "华语流行",
+            "节奏布鲁斯（RnB）"
+        ]
+    },
+    {
+        "Id": 488,
+        "Name": "1022-比尔的歌",
+        "Singer": "Bomb比尔",
+        "Lang": "华语",
+        "MasterPoints": 5,
+        "Notes": "",
+        "DelStatus": 0,
+        "Categories": [
+            "华语流行"
+        ]
+    },
+    {
+        "Id": 716,
+        "Name": "11",
+        "Singer": "队长",
+        "Lang": "华语",
+        "MasterPoints": 3,
+        "Notes": "",
+        "DelStatus": 0,
+        "Categories": [
+            "华语流行"
+        ]
+    },
+    {
+        "Id": 1038,
+        "Name": "180度",
+        "Singer": "孙燕姿",
+        "Lang": "华语",
+        "MasterPoints": 4,
+        "Notes": "",
+        "DelStatus": 0,
+        "Categories": [
+            "华语流行"
+        ]
+    },
+    {
+        "Id": 1153,
+        "Name": "18dB",
+        "Singer": "吴子健REmi,小蓝背心",
+        "Lang": "华语",
+        "MasterPoints": 4,
+        "Notes": "",
+        "DelStatus": 0,
+        "Categories": [
+            "华语流行",
+            "嘻哈（Hip hop）"
+        ]
+    },
+    {
+        "Id": 961,
+        "Name": "271",
+        "Singer": "莫文蔚",
+        "Lang": "华语",
+        "MasterPoints": 5,
+        "Notes": "",
+        "DelStatus": 0,
+        "Categories": [
+            "华语流行"
+        ]
+    },
+    {
+        "Id": 1006,
+        "Name": "5:10a.m.",
+        "Singer": "傻子与白痴",
+        "Lang": "华语",
+        "MasterPoints": 4,
+        "Notes": "",
+        "DelStatus": 0,
+        "Categories": [
+            "华语摇滚"
+        ]
+    },
+    {
+        "Id": 701,
+        "Name": "5：20AM",
+        "Singer": "刀酱版",
+        "Lang": "华语",
+        "MasterPoints": 4,
+        "Notes": "",
+        "DelStatus": 0,
+        "Categories": [
+            "华语流行",
+            "车载音乐"
+        ]
+    }
+]
+
+function getSongs() {
+    return songs;
+}
